d6d1102b761353d0773e9a5804ced3a3516dbdd3
Liudmila Khanina <khaninamila@gmail.com>
2023-11-26 01:29:37 +0300

    Спринт 5/17: 5 → Тема 2/4: Ответственность → Урок 8/9

---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    var delegate: QuestionFactoryDelegate? { get set }
+    
+    func requestNextQuestion()
+}
