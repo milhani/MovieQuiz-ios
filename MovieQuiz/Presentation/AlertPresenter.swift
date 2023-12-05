5e97c73c648c431343db3d38644e150e856790dd
Liudmila Khanina <khaninamila@gmail.com>
2023-12-05 19:38:00 +0300

    Добавила AlertPresenter

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -2,7 +2,29 @@
 //  AlertPresenter.swift
 //  MovieQuiz
 //
-//  Created by Людмила Ханина on 26.11.2023.
-//
 
-import Foundation
+import UIKit
+
+class AlertPresenter {
+    
+    private weak var viewController: UIViewController?
+    
+    init(viewController: UIViewController? = nil) {
+        self.viewController = viewController
+    }
+    
+    func show(alertModel: AlertModel) {
+        let alert = UIAlertController(
+            title: alertModel.title,
+            message: alertModel.message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: alertModel.buttonText, style: .default) { _ in
+            alertModel.completion()
+        }
+        
+        alert.addAction(action)
+        viewController?.present(alert, animated: true, completion: nil)
+    }
+}
+
