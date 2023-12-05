8d0f96c7909d49214795a27557e9966762ca05ea
Liudmila Khanina <khaninamila@gmail.com>
2023-12-05 19:39:00 +0300

    Добавила AlertModel

--- MovieQuiz/Models/AlertModel.swift
+++ MovieQuiz/Models/AlertModel.swift
@@ -2,7 +2,12 @@
 //  AlertModel.swift
 //  MovieQuiz
 //
-//  Created by Людмила Ханина on 26.11.2023.
-//
 
 import Foundation
+
+struct AlertModel {
+  let title: String
+  let message: String
+  let buttonText: String
+  var completion: () -> Void
+}
