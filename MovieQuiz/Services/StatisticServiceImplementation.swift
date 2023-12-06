8cffbc9ec371309cf09d48d7779321b52e8655c6
Liudmila Khanina <khaninamila@gmail.com>
2023-12-06 18:44:18 +0300

    Исправляю проблемы с коммитом

--- MovieQuiz/Services/StatisticServiceImplementation.swift
+++ MovieQuiz/Services/StatisticServiceImplementation.swift
@@ -2,44 +2,7 @@
 //  StatisticServiceImplementation.swift
 //  MovieQuiz
 //
+//  Created by Людмила Ханина on 06.12.2023.
+//
 
 import Foundation
-
-final class StatisticServiceImplementation: StatisticService {
-    
-    private let userDefaults = UserDefaults.standard
-    
-    func store(correct count: Int, total amount: Int) {
-        <#code#>
-    }
-    
-    var totalAccuracy: Double
-    
-    var gamesCount: Int
-    
-    var bestGame: GameRecord {
-        get {
-            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
-            let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
-                return .init(correct: 0, total: 0, date: Date())
-            }
-
-            return record
-        }
-
-        set {
-            guard let data = try? JSONEncoder().encode(newValue) else {
-                print("Невозможно сохранить результат")
-                return
-            }
-
-            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
-        }
-    }
-    
-    
-    private enum Keys: String {
-        case correct, total, bestGame, gamesCount
-    }
-    
-}
