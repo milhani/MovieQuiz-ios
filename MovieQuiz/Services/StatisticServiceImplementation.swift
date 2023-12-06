49af36f640db36e9da47c2d70354bb057e5f7065
Liudmila Khanina <khaninamila@gmail.com>
2023-12-06 18:48:16 +0300

    Исправляю ошибки с коммитом

--- MovieQuiz/Services/StatisticServiceImplementation.swift
+++ MovieQuiz/Services/StatisticServiceImplementation.swift
@@ -2,7 +2,76 @@
 //  StatisticServiceImplementation.swift
 //  MovieQuiz
 //
-//  Created by Людмила Ханина on 06.12.2023.
-//
 
 import Foundation
+
+final class StatisticServiceImplementation: StatisticService {
+    
+    private let userDefaults = UserDefaults.standard
+    
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    
+    var correct: Int {
+        get {
+            userDefaults.integer(forKey: Keys.correct.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.correct.rawValue)
+        }
+    }
+    
+    var total: Int {
+        get {
+            userDefaults.integer(forKey: Keys.total.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.total.rawValue)
+        }
+    }
+
+    var totalAccuracy: Double {
+        return Double(correct) / Double(total) * 100
+    }
+    
+    var gamesCount: Int {
+        get {
+            userDefaults.integer(forKey: Keys.gamesCount.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.gamesCount.rawValue)
+        }
+    }
+    
+    var bestGame: GameRecord {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+            let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                return .init(correct: 0, total: 0, date: Date())
+            }
+
+            return record
+        }
+
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    func store(correct count: Int, total amount: Int) {
+        gamesCount += 1
+        correct += count
+        total += amount
+        
+        let current = GameRecord(correct: count, total: amount, date: Date())
+        if current.correct > bestGame.correct {
+            bestGame = current
+        }
+    }
+}
