db6f4f38f76168d724eb685a0dec62a0aea4e6f3
Liudmila Khanina <khaninamila@gmail.com>
2023-12-07 20:16:56 +0300

    Получение сетевых данных

--- MovieQuiz/Models/MostPopularMovies.swift
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -2,7 +2,22 @@
 //  MostPopularMovies.swift
 //  MovieQuiz
 //
-//  Created by Людмила Ханина on 07.12.2023.
-//
 
 import Foundation
+
+struct MostPopularMovies: Codable {
+    let errorMessage: String
+    let items: [MostPopularMovie]
+}
+
+struct MostPopularMovie: Codable {
+    let title: String
+    let rating: String
+    let imageURL: URL
+    
+    private enum CodingKeys: String, CodingKey {
+    case title = "fullTitle"
+    case rating = "imDbRating"
+    case imageURL = "image"
+    }
+}
--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -2,7 +2,38 @@
 //  MoviesLoader.swift
 //  MovieQuiz
 //
-//  Created by Людмила Ханина on 07.12.2023.
-//
 
 import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
